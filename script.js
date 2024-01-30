ac2214c15b624706b542d167c49c1ce792174fc6
rashid geelani <syed.geelani@vit.edu.in>
2024-01-30 23:32:58 +0530

    l

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 //toggle icon navbar
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll(' header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+}
 
 //scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll(' header nav a');
 window.onscroll = ()=> {
   sections.forEach(sec =>{
     let top = window.scrollY;
